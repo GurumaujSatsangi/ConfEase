af8b61ec9515db1af6ef3cf0c271dcd8a3935adb
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2025-10-13 10:14:18 +0530

    feat: update Google OAuth callback URLs to production; ensure proper authentication flow

--- index.js
+++ index.js
@@ -2158,7 +2158,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/dashboard",
+      callbackURL: "https://confease.onrender.com/auth/google/dashboard",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
@@ -2210,7 +2210,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID3,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET3,
-      callbackURL: "http://localhost:3000/auth3/google/dashboard3",
+      callbackURL: "https://confease.onrender.com/auth3/google/dashboard3",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
@@ -2256,7 +2256,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID2,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET2,
-      callbackURL: "http://localhost:3000/auth2/google/dashboard2",
+      callbackURL: "https://confease.onrender.com/auth2/google/dashboard2",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
