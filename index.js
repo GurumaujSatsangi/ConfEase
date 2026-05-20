ef3d6646813df38753035ca312a45b86abf2ff09
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-05-20 13:52:20 +0530

    updated some email contents

--- index.js
+++ index.js
@@ -2309,7 +2309,7 @@ if(result==true){
     const activation_code = crypto.randomUUID();
     console.log(activation_code);
     await pool.query("insert into activation_requests(email, activation_code) values($1, $2)",[email,activation_code]);
-     sendMail(email,"Welcome to DEI CMT!","Dear "+name+"! Your DEI CMT account has been created succesfully but needs to be activated before you can use it. Please visit https://cmt.gurumaujsatsangi.in/account-activation and enter your Account Activation Code which is "+activation_code+" Incase of any technical assistance please feel free to reach out to us at cmt@dei.ac.in or contact us at +91 9875691340.");
+     sendMail(email,name+", Welcome to DEI CMT!","Dear "+name+"! Your DEI CMT account has been created succesfully but needs to be activated before you can use it. Please visit https://cmt.gurumaujsatsangi.in/account-activation and enter your Account Activation Code which is "+activation_code+" Incase of any technical assistance please feel free to reach out to us at cmt@dei.ac.in or contact us at +91 9875691340.");
 
     return res.redirect("/login/user?message=Your account has been created succesfully, please check your Email inbox for an Email with the subject 'Welcome to DEI CMT!' for the account activation code.");
 
